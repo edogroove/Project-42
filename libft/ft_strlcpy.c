fd3037eb41dc4643a93661b38f72c038470ba958
edogroove <83129479+edogroove@users.noreply.github.com>
2024-01-24 10:06:30 +0000

    upd strlcpy

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -3,34 +3,46 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: parallels <parallels@student.42.fr>        +#+  +:+       +#+        */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 22:25:58 by parallels         #+#    #+#             */
-/*   Updated: 2024/01/23 22:26:17 by parallels        ###   ########.fr       */
+/*   Updated: 2024/01/24 10:03:51 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcpy(char *dest, const char *src, unsigned int size)
+size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 {
-    unsigned int	i;
-    unsigned int	len;
+	unsigned int	i;
+	unsigned int	len;
 
-    i = 0;
-    len = 0;
-    if (dest == NULL && src == NULL)
-        return (0);
-    while (src[len])
-        len++;
-    if (size == 0)
-        return (len);
-    while (src[i] && i < size - 1)
-    {
-        dest[i] = src[i];
-        i++;
-    }
-    if (size != 0)
-        dest[i] = '\0';
-    return (len);
+	i = 0;
+	len = 0;
+	if (dest == NULL && src == NULL)
+		return (0);
+	while (src[len])
+		len++;
+	if (size == 0)
+		return (len);
+	while (src[i] && i < size - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (size != 0)
+		dest[i] = '\0';
+	return (len);
 }
+
+/* int main()
+{
+    char    *src;
+    char    *dest;
+
+    src = "ciao";
+    dest = "";
+    ft_strlcpy(dest, src, 4);
+    printf("%s", dest);
+    return (0);
+} */
--- libft/libft.h
+++ libft/libft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: parallels <parallels@student.42.fr>        +#+  +:+       +#+        */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 15:13:17 by enanni            #+#    #+#             */
-/*   Updated: 2024/01/23 22:33:17 by parallels        ###   ########.fr       */
+/*   Updated: 2024/01/24 09:58:19 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,6 @@ void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
-size_t	ft_strlcpy(char *dest, const char *src, unsigned int size);
+size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 
 #endif
