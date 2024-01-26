57712667b9e262e5b072d418aaf7339a70952f07
edogroove <edoardo.nanni@icloud.com>
2024-01-26 15:11:56 +0100

    stRRchr ok!

--- libft/ft_strrchr.c
+++ libft/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: enanni <enanni@student.42firenze.it>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/26 09:12:01 by enanni            #+#    #+#             */
-/*   Updated: 2024/01/26 09:49:09 by enanni           ###   ########.fr       */
+/*   Updated: 2024/01/26 14:54:33 by enanni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,26 +15,23 @@
 char	*ft_strrchr(const char *s, int c)
 {
 	int	i;
-	int	j;
 
 	i = 0;
-	j = 0;
-	while (s[j] != '\0')
+	while (s[i] != '\0')
+		i++;
+	while (i >= 0)
 	{
-		if ((char)c == s[j])
-			i = j;
-		j++;
+		if (s[i] == (char)c)
+			return ((char *)&s[i]);
+		i--;
 	}
-	if (i == 0)
-		return (0);
-	else
-		return ((char *)&s[i]);
+	return (0);
 }
 
 /* int	main()
 {
-	char s[] = "Ciao, mondo!";
-    char c = '\0';
+	char s[] = "abbbbbbbb";
+    char c = 'a';
     char *p;
 
     p = ft_strrchr(s, c);
