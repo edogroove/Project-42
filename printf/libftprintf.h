8ff9d10df5cb19cd415093b7965ab349be39b052
Edoardo Nanni <enanni@c1r4p8.42firenze.it>
2024-02-20 17:51:57 +0100

    boo

---
+++ printf/libftprintf.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: enanni <enanni@student.42firenze.it>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/20 17:35:48 by enanni            #+#    #+#             */
+/*   Updated: 2024/02/20 17:45:58 by enanni           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+# include <stddef.h>
+
+
+
+
+
+
+
+#endif
