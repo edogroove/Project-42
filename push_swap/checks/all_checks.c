5f48fd46b0baeff3bfeb591662a39a38f7e4fb91
edogroove <83129479+edogroove@users.noreply.github.com>
2024-05-09 20:42:34 +0000

    Revert "da capire se serve l'indice delle liste, makefile ok con i -g"
    This reverts commit 78bce50dfb9c0d91e2adda84233db18c4039f536.
    
    restore

--- push_swap/checks/all_checks.c
+++ push_swap/checks/all_checks.c
@@ -6,7 +6,7 @@
 /*   By: enanni <enanni@student.42firenze.it>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/03 11:39:59 by enanni            #+#    #+#             */
-/*   Updated: 2024/05/09 15:43:56 by enanni           ###   ########.fr       */
+/*   Updated: 2024/05/07 23:43:56 by enanni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,12 @@
 
 int	all_checks(int ac, char **av)
 {
+	int			j;
 	char		**result;
 
 	if (ac == 2)
 	{
+		j = 0;
 		result = av_one_arg(av);
 		if (initial_checks(result) == 1)
 			return (0);
@@ -25,6 +27,9 @@ int	all_checks(int ac, char **av)
 		free_string_array(result);
 	}
 	if (ac > 2)
+	{
+		j = 1;
 		initial_checks_2(av);
+	}
 	return (0);
 }
